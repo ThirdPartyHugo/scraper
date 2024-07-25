c98eb56e5cb115ec7f8e8b0ba16fd497e5e948d5
ThirdPartyHugo <118792142+ThirdPartyHugo@users.noreply.github.com>
2024-07-25 19:05:16 -0400

    Update motkey.py

--- motkey.py
+++ motkey.py
@@ -24,8 +24,7 @@ def search():
     links = soup.find_all("a")
     urls = [clean_url(link.get("href")) for link in links if "url?q=" in link.get("href")]
     for url in urls:
-        print(url)
-    return jsonify(urls)
+        return jsonify(url)
 
 if __name__ == '__main__':
     app.run(debug=True)
